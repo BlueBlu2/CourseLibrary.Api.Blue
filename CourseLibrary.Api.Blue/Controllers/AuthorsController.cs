93ddf55c91177e0d26c7f604f79150e82d7a0488
Blue <73434801+BlueBlu2@users.noreply.github.com>
2021-03-29 01:19:21 +0200

    02. Structuring and Implementing the Outer Facing Contract - Finished -

---
+++ CourseLibrary.Api.Blue/Controllers/AuthorsController.cs
@@ -0,0 +1,34 @@
+﻿using CourseLibrary.API.Blue.Entities;
+using CourseLibrary.API.Blue.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.Api.Blue.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _context;
+
+        public AuthorsController(ICourseLibraryRepository context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Author>> GetAuthors()
+        {
+            return Ok(_context.GetAuthors());
+        }
+        [HttpGet("{authorId:guid}")]
+        public ActionResult<Author> GetAuthor(Guid authorId)
+        {
+            var author = _context.GetAuthor(authorId);
+            return author != null ? Ok(author) : (ActionResult)NotFound();
+        }
+    }
+}
--- CourseLibrary.Api.Blue/Startup.cs
+++ CourseLibrary.Api.Blue/Startup.cs
@@ -27,7 +27,8 @@ namespace CourseLibrary.Api.Blue
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction => setupAction.ReturnHttpNotAcceptable =                    true).AddXmlDataContractSerializerFormatters();
+
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
             services.AddDbContext<CourseLibraryContext>(options =>
